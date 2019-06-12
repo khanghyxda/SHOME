ae9eafd557249389735984918662b2a7d84b0e3e
nguyenhung <mjsakju@gmail.com>
2019-06-12 16:51:11 +0900

    update notice

--- js/notice.js
+++ js/notice.js
@@ -1,5 +1,29 @@
-var password = window.prompt("password","");
 var editor = new Quill('#editor', {
     modules: { toolbar: '#toolbar' },
     theme: 'snow'
+});
+
+$(function () {
+    var password = window.prompt("password", "");
+    var url = "http://localhost:8000/Mail/SendNotice"
+    var data = {
+        password: password,
+        title: $('#title').val(),
+        body: "body",
+    };
+    $("#btnSend").click(function (event) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: data,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (msg) {
+
+            },
+            error: function (error) {
+                alert(error);
+            }
+        });
+    });
 });

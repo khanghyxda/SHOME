23ab3ea9c29efca270ec91815bdbf100fae4d312
nguyenhung <mjsakju@gmail.com>
2020-03-04 11:35:13 +0900

    update vnpay

---
+++ js/vnpay.js
@@ -0,0 +1,64 @@
+var table;
+$(document).ready(function () {
+    table = $('#table').DataTable({
+
+    });
+
+    $("#dtFrom").datepicker({
+        showAnim: "drop",
+        defaultDate: new Date(),
+    });
+    $("#dtFrom").datepicker('setDate', new Date());
+
+    $("#dtTo").datepicker({
+        showAnim: "drop",
+        defaultDate: new Date(),
+    });
+    $("#dtTo").datepicker('setDate', new Date());
+
+    search();
+});
+
+function search() {
+    const data = {
+        from: $("#dtFrom").val(),
+        to: $("#dtTo").val(),
+    };
+    const url = urlApi + "/Payment/GetTemporary"
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json",
+        url: url,
+        cache: false,
+        data: JSON.stringify(data),
+        success: function (data) {
+            console.log(data);
+            updateTable(data);
+        }
+    });
+}
+
+function updateTable(data) {
+    table.clear();
+    var dataSet = [];
+    for (i = 0; i < data.length; i++) {
+        var status = "Chưa thanh toán";
+        if (data[i].temp.status == 1) {
+            status = "Đã thanh toán";
+        }
+        if (data[i].temp.status == 2) {
+            status = "Giao dịch lỗi";
+        }
+        var dataRow = [data[i].temp.paymentId,
+            data[i].temp.mainUserId,
+            data[i].temp.transactionNo,
+            data[i].temp.code,
+            data[i].codeName,
+            status,
+            data[i].time];
+        dataSet.push(dataRow)
+    }
+    table.rows.add(dataSet)
+    table.draw();
+}
